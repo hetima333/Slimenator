572bb790fe382953c8c218d3b17a8e441c47fb70
ヘチマ <hetima333@gmail.com>
2018-10-11 11:22:55 +0900

    [add] add simple manager core

---
+++ Assets/Scripts/Managers/ManagerCore.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerCore : MonoBehaviour {
+
+	// Singleton
+	private static ManagerCore _instance;
+	public static ManagerCore Instance{
+		get{
+			if (_instance == null) {
+				// シーン上から取得する
+				_instance = FindObjectOfType<ManagerCore> ();
+				if (_instance == null) {
+					// ゲームオブジェクトを作成しコンポーネントを追加する
+					_instance = new GameObject ("ObjectPool").AddComponent<ManagerCore>();
+				}
+			}
+			return _instance;
+		}
+	}
+
+	// Manager Properties
+	private HUDManager _hud;
+	public HUDManager Hud{
+		get {
+			if(_hud == null){
+				_hud = GetComponentInChildren<HUDManager>();
+			}
+			return _hud;
+		}
+	}
+
+	private AudioManager _audio;
+	public AudioManager Audio{
+		get {
+			if(_audio == null){
+				_audio = GetComponentInChildren<AudioManager>();
+			}
+			return _audio;
+		}
+	}
+
+	private ObjectManager _objects;
+	public ObjectManager Objects{
+		get {
+			if(_objects == null){
+				_objects = GetComponentInChildren<ObjectManager>();
+			}
+			return _objects;
+		}
+	}
+}
---
+++ Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+ 
+public class Singleton : MonoBehaviour {
+ 
+	// インスタンスが存在するか？
+	static bool existsInstance = false;
+ 
+	// 初期化
+	void Awake () {
+		// インスタンスが存在するなら破棄する
+		if (existsInstance)
+		{
+			Destroy(gameObject);
+			return;
+		}
+ 
+		// 存在しない場合
+		// 自身が唯一のインスタンスとなる
+		existsInstance = true;
+		DontDestroyOnLoad(gameObject);
+	}
+}
