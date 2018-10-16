d7a7a039146025f6a0ff0996049f3f36d10a5c0a
JJ\User <ex-milobur@hotmail.com>
2018-10-16 14:42:20 +0900

    [update] slimebase to be able to take damage.

--- Assets/Scripts/Environment/Slime/SlimeBase/SlimeBase.cs
+++ Assets/Scripts/Environment/Slime/SlimeBase/SlimeBase.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class SlimeBase : MonoBehaviour, ISuckable {
+public abstract class SlimeBase : MonoBehaviour, ISuckable, IDamageable {
 
     SlimeStats _stats;
     SlimeBaseState _state;
