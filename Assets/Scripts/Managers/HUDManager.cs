1757ffe893031d2f28ab56df87ead51dfcbb05b1
ヘチマ <hetima333@gmail.com>
2018-10-17 14:24:09 +0900

    [add] add orb slot (with sample orb images)

--- Assets/Scripts/Managers/HUDManager.cs
+++ Assets/Scripts/Managers/HUDManager.cs
@@ -21,10 +21,20 @@ public class HUDManager : MonoBehaviour {
 
 	private RectTransform _redHPBar;
 	private float _maxWidth;
-		
+
 	#endregion
 
-	
+	#region OrbSlot
+
+	// スロットへの参照
+	private OrbSlot[] _orbSlots = new OrbSlot[3];
+
+	// 仮のコード
+	// TODO : プレイヤーから情報を得る
+	[SerializeField]
+	private Orbs[] _orbs = new Orbs[3];
+
+	#endregion
 
 	// Use this for initialization
 	void Start () {
@@ -34,12 +44,17 @@ public class HUDManager : MonoBehaviour {
 		_redHP = _playerStats.HitPoint;
 		_redHPBar = _playerHPBar.GetComponentInChildren<Image>().rectTransform;
 		_maxWidth = _redHPBar.sizeDelta.x;
+
+		_orbSlots = GameObject.Find("OrbSlot").GetComponentsInChildren<OrbSlot>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// HPBarの更新
 		UpdateHPBar();
+
+		// オーブスロットの更新
+		UpdateOrbSlot();
 	}
 
 	private void UpdateHPBar(){
@@ -58,5 +73,12 @@ public class HUDManager : MonoBehaviour {
 		// HPバーにサイズを適用する
 		_redHPBar.sizeDelta = new Vector2(_redHP * _maxWidth / _playerHPBar.maxValue, _redHPBar.sizeDelta.y);
 
+	}
+
+	private void UpdateOrbSlot(){
+		// オーブのデータを更新する
+		for (int i = 0; i < _orbSlots.Length; i++){
+			_orbSlots[i].Orb = _orbs[i];
+		}
 	}
 }
