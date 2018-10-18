a7b36718136e37c0f7b7fc85e41fceb28cd3b935
ヘチマ <hetima333@gmail.com>
2018-10-18 22:29:13 +0900

    [add] pop damage

---
+++ Assets/Scripts/Managers/HUD/DamagePoper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePoper : SingletonMonoBehaviour<DamagePoper> {
+
+	[SerializeField]
+	private DamageText _dmgText;
+
+	/// <summary>
+	/// ダメージをポップする
+	/// </summary>
+	/// <param name="worldPosition">ポップするワールド座標</param>
+	/// <param name="damage">ダメージ</param>
+	public void PopDamage(Vector3 worldPosition, int damage){
+		// ワールド座標をスクリーン座標に変換する
+		Vector3 pos = Camera.main.WorldToScreenPoint(worldPosition);
+
+		// TODO : オブジェクトプールを使ってインスタンス化する
+		// var ins = Instantiate(_dmgText, pos, Quaternion.identity);
+		var ins = ObjectManager.Instance.InstantiateWithObjectPooling(_dmgText.gameObject, pos);
+		// 親子関係を設定
+		ins.transform.SetParent(this.transform);
+
+		// ダメージをセットする
+		ins.GetComponent<DamageText>().SetDamage(damage);
+
+	}
+}
---
+++ Assets/Scripts/Managers/HUD/DmgPopTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DmgPopTest : MonoBehaviour {
+
+	public GameObject _obj;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			DamagePoper.Instance.PopDamage(_obj.transform.position, Random.Range(1, 201));
+			// Instantiate(_dmg.gameObject, _pos, Quaternion.identity).transform.SetParent(this.transform);
+		}
+
+	}
+}
