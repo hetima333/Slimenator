3457981d23964f889a383527e4776fabc19be553
hetima333 <hetima333@gmail.com>
2018-11-06 12:12:29 +0900

    [update] enchantment tier text

--- Assets/Scripts/HUD/OrbSlot/OrbSlotPresenter.cs
+++ Assets/Scripts/HUD/OrbSlot/OrbSlotPresenter.cs
@@ -21,9 +21,11 @@ public class OrbSlotPresenter : MonoBehaviour {
 	[SerializeField, Header("ExpectedSkill")]
 	private Image _baseElementImage;
 	[SerializeField]
-	private Text _tierText;
+	private Text _baseTierText;
 	[SerializeField]
 	private Image _enchantmentImage;
+	[SerializeField]
+	private Text _enchantmentTierText;
 
 	void Start() {
 		var core = GetComponent<OrbSlotCore>();
@@ -59,15 +61,17 @@ public class OrbSlotPresenter : MonoBehaviour {
 					// スキルがなければSpriteを空にする
 					if (skill == null) {
 						_baseElementImage.sprite = null;
-						_tierText.text = "";
+						_baseTierText.text = "";
 						_enchantmentImage.sprite = null;
+						_enchantmentTierText.text = "";
 					}
 					// ユニークスキル
 					else if (skill.IsUnique()) {
 						// TODO : スキル画像が来たら変更する
 						_baseElementImage.sprite = null;
-						_tierText.text = "unique";
+						_baseTierText.text = "unique";
 						_enchantmentImage.sprite = null;
+						_enchantmentTierText.text = "";
 					}
 					// ユニークでないスキル
 					else {
@@ -75,8 +79,15 @@ public class OrbSlotPresenter : MonoBehaviour {
 						var baseOrb = skill.ToOrb().First();
 
 						_baseElementImage.sprite = OrbToSprite(baseOrb);
-						_tierText.text = tier.ToString();
-						_enchantmentImage.sprite = OrbToSprite(core.Slot.FirstOrDefault(x => x != baseOrb));
+						_baseTierText.text = tier.ToString();
+
+						var enchantmentOrb = core.Slot.FirstOrDefault(x => x != baseOrb);
+						_enchantmentImage.sprite = OrbToSprite(enchantmentOrb);
+						if (enchantmentOrb == Orb.NONE) {
+							_enchantmentTierText.text = "";
+						} else {
+							_enchantmentTierText.text = core.Slot.Skip(1).TakeWhile(x => x == enchantmentOrb).Count().ToString();
+						}
 					}
 				});
 
