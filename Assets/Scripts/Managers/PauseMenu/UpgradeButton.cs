1e1268a32568ed3f6537c4d5233bc2ed9d6b0bdd
hetima333 <hetima333@gmail.com>
2018-11-14 11:25:35 +0900

    [add] upgrade menu

---
+++ Assets/Scripts/Managers/PauseMenu/UpgradeButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UpgradeButton : MenuButtonBase {
+
+	[SerializeField]
+	private Stats _playerStats;
+
+	private enum Upgrade {
+		SPEED,
+		SUCK,
+		HEALTH
+	}
+
+	[SerializeField]
+	private Upgrade _upgrade;
+
+	protected override void OnExecute(PointerEventData e) {
+		switch (_upgrade) {
+			case Upgrade.SPEED:
+				_playerStats.SpeedMultiplyerProperties++;
+				break;
+			case Upgrade.SUCK:
+				_playerStats.SuckingPowerMultiplyerProperties++;
+				break;
+			case Upgrade.HEALTH:
+				_playerStats.HealthMultiplyerProperties++;
+				break;
+		}
+	}
+}
