8a80c7cf37856c9e86bd994894d7dcae033d308f
TSITCL\s162054 <yasu.suika@gmail.com>
2018-11-22 11:16:15 +0900

    [add] boss room

---
+++ Assets/Scripts/RandomMap/MapEvent/WarpPortal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpPortal : MonoBehaviour {
+
+[SerializeField]
+WarpPortal _transferTarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(_transferTarget == null)
+			return;
+
+		if(other.gameObject.tag == "Player")
+		{
+			print("warp!");
+			other.gameObject.transform.position = _transferTarget.transform.position;
+		}
+	}
+}
