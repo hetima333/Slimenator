f770535802cfc5a3fd5752cd00a0932e8732a79e
Nami <jobwave2019@gmail.com>
2018-11-22 11:56:26 +0900

    [delete] unnecessary prefabs in the script

---
+++ Assets/Scripts/RandomMap/MapEvent/CurringTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurringTest : MonoBehaviour {
+
+
+	private Component[] _components;
+
+	// Use this for initialization
+	void Start () {
+		_components = GetComponents<Component>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnWillRenderObject()
+	{
+
+	#if UNITY_EDITOR
+
+		if(Camera.current.name != "SceneCamera"  && Camera.current.name != "Preview Camera")
+
+	#endif
+		{
+			Debug.Log("写ってるはず");
+				}	
+		else
+		{
+		
+		}
+	}	
+
+	// void AllComponentsEnable()
+	// {
+	// 	foreach (var cmp in _components)
+	// 	{
+	// 		cmp.;
+	// 	}
+	// }
+
+
+	// void AllComponentsDisable()
+	// {
+	// 	foreach (var cmp in _components)
+	// 	{
+	// 		cmp.enabled = false;
+	// 	}
+	// }
+}
