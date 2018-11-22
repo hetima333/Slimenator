cc75de1cc95defcb69e058c78a7e1249519c920f
hetima333 <hetima333@gmail.com>
2018-11-22 11:59:00 +0900

    [add] restart button

---
+++ Assets/Scripts/Managers/PauseMenu/ReStartButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// リスタートボタン
+/// </summary>
+public class ReStartButton : MenuButtonBase {
+	protected override void OnExecute(PointerEventData e) {
+		// 現在のシーンのリロード
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		SceneManager.LoadScene("HUD", LoadSceneMode.Additive);
+		// ポーズ解除
+		Pausable.Instance.Pausing = false;
+	}
+}
