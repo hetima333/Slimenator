bdaf1d4eb6f500bd92cf9e000d0518e6f415a590
hetima333 <hetima333@gmail.com>
2018-11-28 15:18:39 +0900

    [update] money dynamic pop

--- Assets/Scripts/Managers/HUDManager.cs
+++ Assets/Scripts/Managers/HUDManager.cs
@@ -1,13 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using UniRx;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class HUDManager : MonoBehaviour {
-
-	private IPlayerStats _playerStats;
 
 	// プレイヤーへの参照
 	private EntityPlayer _player;
@@ -18,20 +13,5 @@ public class HUDManager : MonoBehaviour {
 			}
 			return _player;
 		}
-	}
-
-	// 表示される金額のテキスト
-	[SerializeField]
-	private Text _moneyText;
-
-	// Use this for initialization
-	void Start() {
-		// 所持金が変化したら表示に反映する
-		this.ObserveEveryValueChanged(x => Player.MoneyAmount)
-			.DistinctUntilChanged()
-			.Subscribe(x => {
-				// 表示テキストの更新(3桁ごとにカンマ)
-				_moneyText.text = x.ToString("N0");
-			});
 	}
 }
