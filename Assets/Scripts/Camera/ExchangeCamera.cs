bc851d3c5d3662a883ff4d202858343002e6266d
Nami <jobwave2019@gmail.com>
2018-11-28 18:40:21 +0900

    [add] shooting range of camera.

---
+++ Assets/Scripts/Camera/ExchangeCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExchangeCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //ボス戦時の撮影方法スクリプトはオフ
+        transform.GetChild(0).gameObject.GetComponent<MultipleTargetCamera>().enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            //撮影方法の切り替え
+            ChangeShootingMethod();
+        }
+		
+	}
+
+    /// <summary>
+    /// 撮影方法の切り替え
+    /// </summary>
+    public void ChangeShootingMethod()
+    {
+        //ボス戦時の撮影方法スクリプトはオン
+        transform.GetChild(0).gameObject.GetComponent<MultipleTargetCamera>().enabled = true;
+        //プレイヤー追尾スクリプトはオフ
+        gameObject.GetComponent<CameraFollowTarget>().enabled = false;
+
+    }
+}
---
+++ Assets/Scripts/Camera/MultipleTargetCamera.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MultipleTargetCamera : MonoBehaviour {
+
+    //カメラ本体
+    [SerializeField] private Camera _camera;
+    //カメラの親
+    [SerializeField] private GameObject _camHolder;
+    //カメラの範囲に入れるオブジェクト
+    private List<Transform> _targets;
+
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _smoothTime = 0.5f;
+
+    //範囲
+    [SerializeField] private float _minZoom = 40;
+    [SerializeField] private float _maxZoom = 10;
+    [SerializeField] private float _zoomLimiter = 50;
+
+    private Vector3 _velocity;
+
+    private void Reset()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    void Start()
+    {
+        _targets = new List<Transform>();
+        //プレイヤーをターゲットに追加
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        _targets.Add(player.transform);
+        //ボス達をターゲットに追加  
+        //GameObject boss = GameObject.FindGameObjectWithTag("Boss");
+        GameObject boss = GameObject.Find("KingSlime(Clone)");
+        _targets.Add(boss.transform);
+    }
+
+    private void LateUpdate()
+    {
+        //ターゲットが無ければ終了
+        if (_targets.Count == 0) return;
+
+        //カメラの移動
+        Move();
+        //ターゲットに合わせてズームする
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        var newZoom = Mathf.Lerp(_maxZoom, _minZoom, GetGreatestDistance() / _zoomLimiter);
+        _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        var centerPoint = GetCenterPoint();
+        var newPosition = centerPoint + _offset;
+        _camHolder.transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref _velocity, _smoothTime);
+    }
+
+    private float GetGreatestDistance()
+    {
+        var bounds = new Bounds(_targets[0].position, Vector3.zero);
+        for (int i = 0; i < _targets.Count; i++)
+        {
+            bounds.Encapsulate(_targets[i].position);
+        }
+        return bounds.size.x;
+    }
+
+    private Vector3 GetCenterPoint()
+    {
+        if (_targets.Count == 1) return _targets[0].position;
+        var bounds = new Bounds(_targets[0].position, Vector3.zero);
+        for (int i = 0; i < _targets.Count; i++)
+        {
+            bounds.Encapsulate(_targets[i].position);
+        }
+        return bounds.center;
+    }
+
+}
