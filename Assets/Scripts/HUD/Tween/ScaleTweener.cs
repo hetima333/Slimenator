574d0c533a3131072a7ac25f0de2e991b3633c2c
hetima333 <hetima333@gmail.com>
2018-12-12 10:35:49 +0900

    [add] added tween base

---
+++ Assets/Scripts/HUD/Tween/ScaleTweener.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class ScaleTweener : TweenBase {
+
+	// Use this for initialization
+	void Start() {
+		transform.DOScale(endValue, duration).SetLoops(loops, loopType);
+	}
+}
