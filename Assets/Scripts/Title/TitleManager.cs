584f3bf24052799ed2a060f296b8236fe8ac9474
slovenlyboy <slovenlyboy@gmail.com>
2018-12-18 09:16:00 +0900

    [add]Title BGM

---
+++ Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioManager.Instance.StopBGM();
+		AudioManager.Instance.PlayBGM("TitleBGM",0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
