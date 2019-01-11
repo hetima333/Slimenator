e1f9c10c3ad1b613fba85b50605abc10a617780d
hetima333 <hetima333@gmail.com>
2019-01-11 14:37:28 +0900

    [fix] fixed player to point in the right direction

--- Assets/Scripts/Player/PlayerInput/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerInput/PlayerMover.cs
@@ -12,6 +12,9 @@ public class PlayerMover : MonoBehaviour {
 	[SerializeField]
 	private Camera _camera;
 
+	// 視点変更用の床
+	Plane plane = new Plane();
+
 	void Start() {
 		var input = GetComponent<IPlayerInput>();
 		var rb = GetComponent<Rigidbody>();
@@ -35,10 +38,18 @@ public class PlayerMover : MonoBehaviour {
 			.Where(_ => player.GetPlayerState() != EnumHolder.States.DIE)
 			.DistinctUntilChanged()
 			.Subscribe(position => {
-				// マウス移動量を見てその方向を向く
-				var target = camera.ScreenToWorldPoint(position);
-				target.y = transform.position.y;
-				transform.LookAt(target);
+				// カメラとマウスの位置を元にRayを準備
+				var ray = Camera.main.ScreenPointToRay(position);
+				float distance = 0;
+
+				// プレイヤーの高さにPlaneを更新して、カメラの情報を元に地面判定して距離を取得
+				plane.SetNormalAndPosition(Vector3.up, transform.localPosition);
+				if (plane.Raycast(ray, out distance)) {
+
+					// 距離を元に交点を算出して、交点の方を向く
+					var lookPoint = ray.GetPoint(distance);
+					transform.LookAt(lookPoint);
+				}
 			});
 
 		// 移動方向用のカメラをプレイヤーに追尾
