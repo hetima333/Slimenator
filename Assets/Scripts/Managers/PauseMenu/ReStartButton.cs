583786cb01a0686a404e700e07b4a235558508c4
slovenlyboy <slovenlyboy@gmail.com>
2019-01-31 17:15:42 +0900

    [fix]all scene font

--- Assets/Scripts/Managers/PauseMenu/ReStartButton.cs
+++ Assets/Scripts/Managers/PauseMenu/ReStartButton.cs
@@ -9,7 +9,12 @@ public class ReStartButton : MenuButtonBase {
 	protected override void OnExecute(PointerEventData e) {
 		// 現在のシーンのリロード
 		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-		SceneManager.LoadScene("HUD", LoadSceneMode.Additive);
+		//Tutorialシーンでは専用HUDを使用するのでプレイシーンのみHUD差異読み込む
+		if (SceneManager.GetActiveScene().name == "PlayTestScene") {
+					
+					SceneManager.LoadScene("HUD", LoadSceneMode.Additive);
+				}
+		
 		// ポーズ解除
 		if(Pausable.Instance!=null)
 		Pausable.Instance.Pausing = false;
--- Assets/Scripts/Title/TitleButton.cs
+++ Assets/Scripts/Title/TitleButton.cs
@@ -7,16 +7,13 @@ public class TitleButton : MenuButtonBase {
 	[SerializeField]
 	private string _sceneName;
 
-	[SerializeField]
-	private string _HUD;
-
 	protected override void OnExecute (PointerEventData e) {
 		//決定SE
 		AudioManager.Instance.PlaySE("Decide",false,1);
 		SceneManager.LoadScene (_sceneName);
 		if(_sceneName != "TutorialScene")
 		{
-			Application.LoadLevelAdditive (_HUD);
+			SceneManager.LoadScene("HUD", LoadSceneMode.Additive);
 		}
 		
 	}
