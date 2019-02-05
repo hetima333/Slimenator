1d19189056184c6b3017f6cbc89aa06f0a59401e
hetima333 <hetima333@gmail.com>
2019-02-05 23:42:10 +0900

    [update] display upgrade count

---
+++ Assets/Scripts/HUD/UpgradeCountPresenter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeCountPresenter : MonoBehaviour {
+
+	[SerializeField]
+	private Text _healthUpgradeText;
+
+	[SerializeField]
+	private Text _skillUpgradeText;
+
+	[SerializeField]
+	private Stats _playerStats;
+
+	// Use this for initialization
+	void Start() {
+		
+		// HPのアップグレード段階を更新
+		_playerStats.ObserveEveryValueChanged(x => x.HealthMultiplyerProperties)
+			.Subscribe(x => {
+				_healthUpgradeText.text = ((int) ((_playerStats.HealthMultiplyerProperties - 1) / 0.2f + 1)).ToString();
+			});
+
+		// スキルのアップグレード段階を更新
+		_playerStats.ObserveEveryValueChanged(x => x.SkillPowerMultiplyerProperties)
+			.Subscribe(x => {
+				_skillUpgradeText.text = ((int) ((_playerStats.SkillPowerMultiplyerProperties - 1) / 0.2f + 1)).ToString();
+			});
+	}
+}
