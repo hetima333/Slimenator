7c422d37390b5337b42424ba90ee830aff665c3b
slovenlyboy <slovenlyboy@gmail.com>
2019-02-06 17:40:29 +0900

    [fix]Credit

---
+++ Assets/Scripts/Title/ChangeCredit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ChangeCredit : MenuButtonBase {
+
+	[SerializeField]
+	private GameObject _creditPanel;
+
+	[SerializeField]
+	private GameObject _otherCreditPanel;
+
+
+	protected override void OnExecute (PointerEventData e) {
+		//決定SE
+		AudioManager.Instance.PlaySE("Decide",false,1);
+		if(_creditPanel.activeSelf == true)
+		{
+			_otherCreditPanel.SetActive(true);
+			_creditPanel.SetActive(false);
+		}
+		else
+		{
+			_creditPanel.SetActive(true);
+			_otherCreditPanel.SetActive(false);
+		}
+	
+		
+	}
+}
